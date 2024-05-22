a92a4da27c867a4843a3c7ba57a558421858f883
Alexis Romero Mendoza <al162917@alumnos.uacj.mx>
2024-05-22 11:23:54 -0600

    working with models of Hdeleon

--- mob_mendoza/admin.py
+++ mob_mendoza/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from .models import Forniture, FornitureCategory,FornitureCombos, Product, ShoppingCart,PurchaseOrder, DetailedOrder, Address, Client
+from .models import Forniture, FornitureCategory,FornitureCombos, Product
 
 # Register your models here.
 admin.site.register(Forniture)
 admin.site.register(FornitureCategory)
 admin.site.register(Product)
-admin.site.register(ShoppingCart)
-admin.site.register(PurchaseOrder)
+
+
 admin.site.register(FornitureCombos)
-admin.site.register(DetailedOrder)
-admin.site.register(Address)
-admin.site.register(Client)
--- mob_mendoza/serializer.py
+++ mob_mendoza/serializer.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from .models import Forniture, Product, Client, Address, ShoppingCart, PurchaseOrder, DetailedOrder, CartItem
+from .models import Forniture, Customer, Product, Cart, Address, Order, OrderDetail
 
 class FornitureSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Forniture
-        # fields = ('id', 'tipo')
+        model = Forniture        
+        fields = '__all__'
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -12,9 +16,9 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
-class ClientSerializer(serializers.ModelSerializer):
+class CartSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Client
+        model = Cart
         fields = '__all__'
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -22,42 +26,12 @@ class AddressSerializer(serializers.ModelSerializer):
         model = Address
         fields = '__all__'
 
-class CartItemSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CartItem
+        model = Order
         fields = '__all__'
 
-class ShoppingCartSerializer(serializers.ModelSerializer):    
+class OrderDetailedSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ShoppingCart
-        fields = ['id', 'client', 'created_at']
-
-class DetailedOrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-
-    class Meta:
-        model = DetailedOrder
-        fields = ['id','order', 'product', 'quantity', 'cost']
-
-class PurchaseOrderSerializer(serializers.ModelSerializer):
-    details = DetailedOrderSerializer(many=True)
-    
-    class Meta:
-        model = PurchaseOrder
-        fields = ['id', 'client', 'created_at', 'status', 'details']
-
-class OrderCreateSerializer(serializers.ModelSerializer):
-    details = DetailedOrderSerializer(many=True)
-
-    class Meta:
-        model = PurchaseOrder
-        fields = ['client', 'details']
-
-    def create(self, validated_data):
-        details_data = validated_data.pop('details')
-        order = PurchaseOrder.objects.create(**validated_data)
-        for detail_data in details_data:
-            product = Product.objects.get(id=detail_data['product']['id'])
-            DetailedOrder.objects.create(order=order, product = product, **detail_data)
-        return order
-
+        model = OrderDetail
+        fields = '__all__'
